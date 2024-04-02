996580a6d8f1fbc040a4b2bc02cc181be68d7886
syagawa <syagawapp@gmail.com>
2024-04-02 22:57:21 +0900

    Add tinyusb-msc-settings

---
+++ tinyusb-msc-settings/main/app.h
@@ -0,0 +1,35 @@
+#ifndef APP_H
+#define APP_H
+
+#include "storage.h"
+#include "button.h"
+
+
+static void buttonAction1(){
+  char * color = getButtonColor();
+  showColorWithBrightness(color, brightness_test);
+  brightness_test = brightness_test - 0.1;
+  if(brightness_test < 0){
+    brightness_test = 1.0;
+  }
+}
+
+static void buttonAction2(){
+    ESP_LOGI(TAG, "double clicked!");
+}
+
+void setApp(){
+    // write initialize for app
+    char * color = getSettingByKey("color");
+    setButtonColor(color);
+    ESP_LOGI(TAG, "Color: %s!", color);
+
+    singleClickAction = buttonAction1;
+    doubleClickAction = buttonAction2;
+}
+
+void appInLoop(){
+    // write logic in loop
+}
+
+#endif // APP_H
