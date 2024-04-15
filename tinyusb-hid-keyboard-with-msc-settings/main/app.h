ec76b2e6f1fe5109a39109452954f13b5c6f7c02
syagawa <syagawapp@gmail.com>
2024-04-16 00:47:38 +0900

    Add tinyusb-hid-keyboard-with-msc-settings

---
+++ tinyusb-hid-keyboard-with-msc-settings/main/app.h
@@ -0,0 +1,43 @@
+#ifndef APP_H
+#define APP_H
+
+#include "storage.h"
+#include "button.h"
+
+
+const char * initialDataStr = "{\"settings_mode\": \"storage\", \"color\": \"red\"}";
+const char * versionStr = "tinyusb-msc-settings-1.2.0";
+
+static void buttonAction1(){
+  char * color = getButtonColor();
+  showColorWithBrightness(color, brightness_test);
+  brightness_test = brightness_test - 0.1;
+  if(brightness_test < 0){
+    brightness_test = 1.0;
+  }
+}
+
+static void buttonAction2(){
+  ESP_LOGI(TAG, "double clicked!");
+}
+
+static void buttonAction3(){
+  ESP_LOGI(TAG, "long pressed!");
+}
+
+void setApp(){
+    // write initialize for app
+    char * color = getSettingByKey("color");
+    setButtonColor(color);
+    ESP_LOGI(TAG, "Color: %s!", color);
+
+    singleClickAction = buttonAction1;
+    doubleClickAction = buttonAction2;
+    longPressedAction = buttonAction3;
+}
+
+void appInLoop(){
+    // write logic in loop
+}
+
+#endif // APP_H
---
+++ tinyusb-hid-keyboard-with-msc-settings/main/main.c
@@ -0,0 +1,97 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <errno.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include "esp_partition.h"
+#include "esp_check.h"
+#include "tinyusb.h"
+#include "tusb_msc_storage.h"
+#include "tusb_cdc_acm.h"
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+
+// #include "iot_button.h"
+
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// #include "jsmn.h"
+#include "cJSON.h"
+
+#include "led.h"
+#include "button.h"
+#include "storage.h"
+#include "app.h"
+
+
+
+void firstWait(int sec){
+
+  int delayMS = 10;
+  int ms = 1000;
+  int waitMS = sec * ms;
+
+  TickType_t waitStartTime = xTaskGetTickCount();
+
+  ESP_LOGI(TAG, "firstWait0 %ld", xTaskGetTickCount());
+
+  while((xTaskGetTickCount() - waitStartTime) <= pdMS_TO_TICKS(waitMS)){
+
+    if(pushedBtnLong == 1){
+      setButtonLongPressInited();
+      setBootModeSettings();
+      ESP_LOGI(TAG, "firstWait1 %ld", xTaskGetTickCount());
+      break;
+    }
+    vTaskDelay(delayMS / portTICK_PERIOD_MS);
+
+  }
+
+  ESP_LOGI(TAG, "firstWait2 %ld", xTaskGetTickCount());
+
+  setCompletedFirstWait();
+}
+
+
+
+void enterSettingsMode(){
+  lightLed("WHITE");
+  startSettingsMode();
+}
+
+void enterMain(){
+  setApp();
+  while(1){
+    appInLoop();
+    vTaskDelay(1);
+  }
+}
+
+void app_main(void){
+
+    initButton();
+    initLed();
+    initSettings(versionStr, initialDataStr);
+
+    firstWait(3);
+    ESP_LOGI(TAG, "bootmode %d", getBootMode());
+
+    if(isBootModeSettings()){
+      enterSettingsMode();
+    }else{
+      enterMain();
+    }
+
+}
