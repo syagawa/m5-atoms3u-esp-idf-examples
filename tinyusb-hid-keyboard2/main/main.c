460e5e6c189e96c704177170dd52b152e910d52c
syagawa <syagawapp@gmail.com>
2025-12-18 00:15:36 +0900

    Add new logic of inputing key

--- tinyusb-hid-keyboard2/main/main.c
+++ tinyusb-hid-keyboard2/main/main.c
@@ -17,7 +17,7 @@
 #include "esp_err.h"
 #include "usb_descriptors.h"
 #include "device/usbd.h"
-
+// #include "keyboard.h"
 
 #define TAG "HID Example1"
 static bool s_keyboard_pressed = false;
@@ -254,6 +254,105 @@ static void button_km_cb(void *arg, void *arg2){
 }
 
 
+
+static void ascii_to_hid_with_modifier(char c, uint8_t *keycode, uint8_t
+*modifier) {
+     *modifier = 0;
+     *keycode = 0;
+
+     // アルファベット小文字
+     if (c >= 'a' && c <= 'z') {
+         *keycode = (c - 'a') + HID_KEY_A;
+     }
+     // アルファベット大文字
+     else if (c >= 'A' && c <= 'Z') {
+         *keycode = (c - 'A') + HID_KEY_A;
+         *modifier = KEYBOARD_MODIFIER_LEFTSHIFT;
+     }
+     // 数字
+     else if (c >= '1' && c <= '9') {
+         *keycode = (c - '1') + HID_KEY_1;
+     }
+     else if (c == '0') {
+         *keycode = HID_KEY_0;
+     }
+     // 記号と特殊キー (US配列基準)
+     else {
+         switch (c) {
+             case ' ':  *keycode = HID_KEY_SPACE; break;
+             case '\n': *keycode = HID_KEY_ENTER; break;
+             case '\t': *keycode = HID_KEY_TAB; break;
+             case '-':  *keycode = HID_KEY_MINUS; break;
+             case '=':  *keycode = HID_KEY_EQUAL; break;
+             case '[':  *keycode = HID_KEY_BRACKET_LEFT; break;
+             case ']':  *keycode = HID_KEY_BRACKET_RIGHT; break;
+             case '\\': *keycode = HID_KEY_BACKSLASH; break;
+             case ';':  *keycode = HID_KEY_SEMICOLON; break;
+             case '\'': *keycode = HID_KEY_APOSTROPHE; break;
+             case '`':  *keycode = HID_KEY_GRAVE; break;
+             case ',':  *keycode = HID_KEY_COMMA; break;
+             case '.':  *keycode = HID_KEY_PERIOD; break;
+             case '/':  *keycode = HID_KEY_SLASH; break;
+
+             // Shiftが必要な記号
+             case '!':  *keycode = HID_KEY_1; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '@':  *keycode = HID_KEY_2; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '#':  *keycode = HID_KEY_3; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '$':  *keycode = HID_KEY_4; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '%':  *keycode = HID_KEY_5; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '^':  *keycode = HID_KEY_6; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '&':  *keycode = HID_KEY_7; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '*':  *keycode = HID_KEY_8; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '(':  *keycode = HID_KEY_9; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case ')':  *keycode = HID_KEY_0; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '_':  *keycode = HID_KEY_MINUS; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '+':  *keycode = HID_KEY_EQUAL; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '{':  *keycode = HID_KEY_BRACKET_LEFT; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '}':  *keycode = HID_KEY_BRACKET_RIGHT; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '|':  *keycode = HID_KEY_BACKSLASH; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case ':':  *keycode = HID_KEY_SEMICOLON; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '\"': *keycode = HID_KEY_APOSTROPHE; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '~':  *keycode = HID_KEY_GRAVE; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '<':  *keycode = HID_KEY_COMMA; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '>':  *keycode = HID_KEY_PERIOD; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+             case '?':  *keycode = HID_KEY_SLASH; *modifier = KEYBOARD_MODIFIER_LEFTSHIFT; break;
+
+             default:   *keycode = 0; break; // 未対応
+         }
+     }
+}
+
+void usb_hid_print_string(const char *str) {
+     uint8_t key_none[6] = {0};
+
+     for (int i = 0; str[i] != '\0'; i++) {
+         uint8_t keycode = 0;
+         uint8_t modifier = 0;
+
+         ascii_to_hid_with_modifier(str[i], &keycode, &modifier);
+
+         if (keycode != 0) {
+             uint8_t key_report[6] = {keycode, 0, 0, 0, 0, 0};
+
+             // キーを押す
+             tinyusb_hid_keyboard_report(key_report, modifier);
+             vTaskDelay(pdMS_TO_TICKS(30));
+
+             // キーを離す
+             tinyusb_hid_keyboard_report(key_none, 0);
+             vTaskDelay(pdMS_TO_TICKS(30));
+         }
+     }
+}
+
+
+
+
+static void button_km_cb2(void *arg, void *arg2) {
+     // 記号混じりの文字列も送信可能
+     usb_hid_print_string("User: ESP32-S3!\nPassword: Admin_123");
+}
+
 //--------------------------------------------------------------------+
 // Device callbacks
 //--------------------------------------------------------------------+
@@ -341,7 +440,7 @@ void app_main(void)
 //     ESP_LOGI(TAG, "HID Mouse demo: press button to simulate mouse");
 // #endif
 
-    button_cb_t button_cb = button_km_cb;
+    button_cb_t button_cb = button_km_cb2;
 
     ESP_LOGI(TAG, "Wait Mount through USB interface");
 
